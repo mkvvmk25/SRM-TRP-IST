ee8271016729ef0342c39cc857ead16214e4c92a
mkvvmk25 <152393796+mkvvmk25@users.noreply.github.com>
2024-07-30 15:59:52 +0530

    Create jsonoops.js

---
+++ jsonoops.js
@@ -0,0 +1,111 @@
+// let obj = {
+//   "key": 64,
+//   "hello world": 101,
+//   78: "hello",
+//   "arr": [7, 9.54, true],
+// };
+
+// console.log(obj.key);
+// console.log(obj["hello world"]);
+// console.log(obj[78]);
+// console.log(  obj.78 );
+
+// properties
+// functions or methods
+// let Person = {
+//   fname: "raj",
+//   age: 32,
+//   //   "aboutMe": function ()
+//   //   {
+//   //     console.log(`i am ${this.fname}, age is ${this.age}`);
+//   //   }
+// };
+
+// // person.prototype.aboutMe = function () {
+// //   console.log(`i am ${this.fname}, age is ${this.age}`);
+// // };
+
+// console.log("Person",78 );
+// console.log(JSON.stringify(Person));
+// // person.aboutMe();
+
+// OOPS
+/* 
+  to work with class and object 
+  -> classes can be created in 3 ways 
+        -> constructor function 
+        -> ES6 classes
+        -> Object.create() 
+*/
+
+// let Human = function (fnameVal, ageVal) {
+//   this.fname = fnameVal;
+//   this.age = ageVal;
+// };
+
+// Human.prototype.state = "tamilnadu";
+// Human.prototype.aboutMe = function () {
+//   console.log(`i am ${this.fname}, age is ${this.age}`);
+// };
+
+// create object
+// let raj = new Human("raj", 25);
+// raj.aboutMe();
+// console.log(raj);
+
+// let kiran = new Human("kiran", 32);
+
+// kiran.fname = "kiran";
+// kiran.age = 25;
+// let kumar = new Human();
+// let tarun = new Human();
+// console.log(kiran);
+// console.log(kumar);
+
+/* 
+  pillars of OOPS 
+    --Inheritance
+    encapsulation 
+    abstraction 
+    polymorphsim 
+*/
+
+// let Student = {    // json
+//   "fname":"none",
+//   "rollNum":0
+// }
+
+// let tarun = Object.create(Student);
+// console.log(tarun);
+
+let Human = function (fnameVal, ageVal) {
+  this.fname = fnameVal;
+  this.age = ageVal;
+};
+
+Human.prototype.state = "tamilnadu";
+Human.prototype.aboutMe = function () {
+  console.log(`i am ${this.fname}, age is ${this.age}`);
+};
+
+let Student = function (rollVal, fnameVal, ageVal, dptVal) {
+  // call HumAN CLASS CONSTRUCTOR funtion under child class
+  Human.call(this, fnameVal, ageVal);
+  this.rollNum = rollVal;
+  this.department = dptVal;
+};
+
+
+// linking prototypes
+Student.prototype = Object.create(Human.prototype);
+
+
+// Student.prototype = Human.prototype;
+
+
+Student.prototype.studying = function()
+{
+    console.log(`i study ${this.department}`);
+}
+let raj = new Student(101, "raj", 20, "CSE");
+console.log(raj);
